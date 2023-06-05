2973e03a54685fd971323375ada4fa83a998e8c9
ADITYA2777 <ajadityajain01@gmail.com>
2023-06-05 11:45:35 -0700

    ecommerce website

---
+++ index.js
@@ -0,0 +1,10 @@
+let navside = document.getElementById('navside')
+let menubar = document.getElementById('menubar')
+navside.style.width="0px";
+menubar.onclick=function(){
+    if(navside.style.width=="0px"){
+        navside.style.width="250px"
+    }else{
+        navside.style.width="0px"
+    }
+}
